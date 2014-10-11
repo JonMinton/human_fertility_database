02596a09e4755cec2832bc9b6485c159f4fa4b33
Jonathan Minton <nate.minton@gmail.com>
2014-10-11 18:04:09 +0100

    first commit

---
+++ script.r
@@ -0,0 +1,15 @@
+rm(list=ls())
+
+source("scripts/LoadPackages.R")
+
+RequiredPackages(
+  c(
+    "r2stl",
+    "ggplot2",
+    "reshape2",
+    "plyr",
+    "lattice",
+    "stringr"
+    )
+  )
+
